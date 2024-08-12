2a88b1186df16e6af5027c59e312b1dba86d9296
KalimeroMK <zbogoevski@gmail.com>
2024-08-12 21:08:33 +0200

    update seo

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,18 +8,18 @@
     </title>
     <!-- Dynamic Meta Tags -->
     <meta name="description" content="{{ !empty($article->description) ? Str::limit(strip_tags($article->description)
-    , 200) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years of professional
+    , 150) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years of professional
     experience. Get in touch to discuss your next project.' }}">
     <meta property="og:title" content="{{ !empty($article->title) ? $article->title : 'CV: Zoran Bogoevski | Senior Software Engineer | Laravel Developer' }}">
     <meta property="og:description" content="{{ !empty($article->description) ? Str::limit(strip_tags
-    ($article->description), 200) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years
+    ($article->description), 150) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years
     of professional experience. Get in touch to discuss your next project.' }}">
     <meta property="og:image" content="{{ !empty($article->image) ? asset('storage/' . $article->image) : 'https://zbogoevski.github.io/images/social-share.png' }}">
     <meta property="og:url" content="{{ !empty($article) ? route('article', $article->id) : 'https://zorandev.info' }}">
     <meta name="og:type" content="article">
     <meta name="twitter:title" content="{{ !empty($article->title) ? $article->title : 'CV: Zoran Bogoevski | Senior Software Engineer | Laravel Developer' }}">
     <meta name="twitter:description" content="{{ !empty($article->description) ? Str::limit(strip_tags
-    ($article->description), 200) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years
+    ($article->description), 150) : 'Zoran Bogoevski, a Senior Software Engineer and Laravel Developer with 10+ years
     of professional experience. Get in touch to discuss your next project.' }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style-cf.css') }}">
