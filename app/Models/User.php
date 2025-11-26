44958cfa47d7b7c47cb3362c4ec5b978c86e7cd8
KalimeroMK <zbogoevski@gmail.com>
2025-11-26 23:17:49 +0100

    Add array cast for custom_fields in User model to fix Filament 4 compatibility

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,7 @@ class User extends Authenticatable implements FilamentUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
+            'custom_fields' => 'array',
         ];
     }
 }
